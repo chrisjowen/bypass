94194857b62126ec34329eaec727e49ba95924c3
Chris Owen <chris.j.owen@hotmail.co.uk>
2017-03-14 12:40:38 +0000

    Removed ExUnit callback

--- lib/bypass.ex
+++ lib/bypass.ex
@@ -9,20 +9,20 @@ defmodule Bypass do
       {:ok, pid} ->
         port = Bypass.Instance.call(pid, :port)
         debug_log "Did open connection #{inspect pid} on port #{inspect port}"
-        ExUnit.Callbacks.on_exit({Bypass, pid}, fn ->
-          case Bypass.Instance.call(pid, :on_exit) do
-            :ok ->
-              :ok
-            :ok_call ->
-              :ok
-            {:error, :not_called} ->
-              raise ExUnit.AssertionError, "No HTTP request arrived at Bypass"
-            {:error, :unexpected_request} ->
-              raise ExUnit.AssertionError, "Bypass got an HTTP request but wasn't expecting one"
-            {:exit, {class, reason, stacktrace}} ->
-              :erlang.raise(class, reason, stacktrace)
-          end
-        end)
+        # ExUnit.Callbacks.on_exit({Bypass, pid}, fn ->
+        #   case Bypass.Instance.call(pid, :on_exit) do
+        #     :ok ->
+        #       :ok
+        #     :ok_call ->
+        #       :ok
+        #     {:error, :not_called} ->
+        #       raise ExUnit.AssertionError, "No HTTP request arrived at Bypass"
+        #     {:error, :unexpected_request} ->
+        #       raise ExUnit.AssertionError, "Bypass got an HTTP request but wasn't expecting one"
+        #     {:exit, {class, reason, stacktrace}} ->
+        #       :erlang.raise(class, reason, stacktrace)
+        #   end
+        # end)
         %Bypass{pid: pid, port: port}
       other ->
         other
